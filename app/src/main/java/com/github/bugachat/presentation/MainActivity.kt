b9e68a8c36b77684b3d0121d52225e18b64bd7d9
Stanislao <stanis.campregher@gmail.com>
2025-04-18 19:24:48 -0300

    Adding Firebase Auth with google

--- app/src/main/java/com/github/bugachat/presentation/MainActivity.kt
+++ app/src/main/java/com/github/bugachat/presentation/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.github.bugachat.presentation
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -12,19 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
-import androidx.credentials.CredentialManager
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.exceptions.GetCredentialException
 import com.github.bugachat.commom.GoogleSignInConfiguration
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
-import com.google.firebase.firestore.firestoreSettings
-import kotlinx.coroutines.runBlocking
-import java.security.MessageDigest
-import java.util.UUID
 
 class MainActivity : ComponentActivity() {
 
